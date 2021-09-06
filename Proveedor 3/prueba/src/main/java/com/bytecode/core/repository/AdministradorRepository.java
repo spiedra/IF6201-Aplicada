c5d7de7fc7ec518f62c96098ad51561ebe2d7526
AracelySC <arasaenz15@gmail.com>
2021-09-06 00:01:26 -0600

    Crud Producto

--- Proveedor 3/prueba/src/main/java/com/bytecode/core/repository/AdministradorRepository.java
+++ Proveedor 3/prueba/src/main/java/com/bytecode/core/repository/AdministradorRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bytecode.core.model.Administrador;
-import com.bytecode.core.model.Employee;
 
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Integer>{
---
+++ Proveedor 3/prueba/src/main/java/com/bytecode/core/service/ProductoService.java
@@ -0,0 +1,14 @@
+package com.bytecode.core.service;
+
+import java.util.List;
+
+import com.bytecode.core.model.Producto;
+
+
+
+public interface ProductoService {
+    List <Producto> obtenerProductos();
+    void saveProducto(Producto producto);
+    Producto getProductoById(int id);
+    void deleteProductoById(int id);
+}
