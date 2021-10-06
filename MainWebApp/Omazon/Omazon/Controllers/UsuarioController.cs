83d5c75806e9927b0ec1d6f2bb4f63184f045cc7
Fabricio-A-E <carlos.agueroelizondo@ucr.ac.cr>
2021-10-05 21:08:56 -0600

    Página principal y carrito

---
+++ MainWebApp/Omazon/Omazon/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Omazon.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Carrito()
+        {
+            return View();
+        }
+    }
+}
