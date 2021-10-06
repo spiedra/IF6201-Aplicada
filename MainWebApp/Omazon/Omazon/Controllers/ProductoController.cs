f226698f5a770f89ee15c632f8d2b65444efb782
Fabricio-A-E <carlos.agueroelizondo@ucr.ac.cr>
2021-10-05 22:01:41 -0600

    Ver información producto

---
+++ MainWebApp/Omazon/Omazon/Controllers/ProductoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Omazon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Omazon.Controllers
+{
+    public class ProductoController : Controller
+    {
+        [HttpPost]
+        public IActionResult Ver(ProductoModel producto)
+        {
+            ViewBag.nombre = producto.Nombre;
+            return View();
+        }
+    }
+}
