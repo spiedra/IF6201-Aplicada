c5e980e5c0be7664b9d2549acaaa64e018dde519
spiedra <jcspiedra@gmail.com>
2021-10-05 21:19:24 -0600

    modal custom added

--- Proveedor 1/frontend/src/Views/RegisterCategory/RegisterCategory.js
+++ Proveedor 1/frontend/src/Views/RegisterCategory/RegisterCategory.js
@@ -5,46 +5,53 @@ import { RegisterCategoryPost } from "../../services/testApi";
 import Menu from "../../commons/Menu/Menu";
 import FormDefault from "../../commons/FormDefault/FormDefault";
 import "./RegisterCategory.css";
-import ModalAlert from "../../commons/ModalAlert/ModalAlert";
+import ModalCustom from "../../commons/ModalCustom/ModalCustom";
 
 const RegisterCategory = () => {
   const [categoryName, setCategoryName] = useState("");
 
-  // Alert
-  const [show, setShow] = useState(true);
-  const handleClose = () => setShow(false);
+  // Modal
+  const [show, setShow] = useState(false);
+  const [message, setMessage] = useState("");
 
   function handleChange(name, value) {
     setCategoryName(value);
   }
 
   function handleSubmit() {
-    // let category = { categoryName };
-    // if (category.categoryName !== null) {
-    //   RegisterCategoryPost(category).then((data) => {
-    //     <ModalAlert attribute={{message: "Categoría registrada correctamente"}} />
-    //   });
-    // }
-    console.log("jcspi");
-    setShow(true);
-    <ModalAlert attribute={{message: "Categoría registrada correctamente", show: show}} handleClose={handleClose} />
+    let category = { categoryName };
+    if (category.categoryName !== null) {
+      RegisterCategoryPost(category).then((data) => {
+        setMessage("Categoría registrada correctamente");
+        setShow(true);
+      });
+    }
   }
 
   return (
-    <div className="container_form-category">
-      <Menu titleName="HachePe" />
-      <Container className="m-0 mt-4 ms-3">
-        <h1>Registro categoría</h1>
-        <hr className="h1" />
-      </Container>
-      <Container className="category__container px-0">
-        <FormDefault
-          attribute={{ name: "categoría", action: "Registrar" }}
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
-        />
-      </Container>
-    </div>
+    <>
+      <div className="container_form-category">
+        <Menu titleName="HachePe" />
+        <Container className="m-0 mt-4 ms-3">
+          <h1>Registro categoría</h1>
+          <hr className="h1" />
+        </Container>
+        <Container className="category__container px-0">
+          <FormDefault
+            attribute={{ name: "categoría", action: "Registrar" }}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+          />
+        </Container>
+      </div>
+      <ModalCustom
+        attribute={{
+          message: message,
+          show: show,
+          handleClose: () => setShow(false),
+        }}
+      />
+    </>
   );
 };
 
--- Proveedor 1/frontend/src/commons/ModalCustom/ModalCustom.jsx
+++ Proveedor 1/frontend/src/commons/ModalCustom/ModalCustom.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
-const ModalAlert = ({ attribute, handleClose}) => {
+const ModalCustom = ({ attribute }) => {
   return (
     <>
-      <Modal show={attribute.show} onHide={handleClose}>
+      <Modal show={attribute.show} onHide={attribute.handleClose}>
         <Modal.Header closeButton>
-          {/* <Modal.Title>Modal heading</Modal.Title> */}
         </Modal.Header>
         <Modal.Body>{attribute.message}</Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={attribute.handleClose}>
             Aceptar
           </Button>
         </Modal.Footer>
@@ -19,4 +18,4 @@ const ModalAlert = ({ attribute, handleClose}) => {
   );
 };
 
-export default ModalAlert;
+export default ModalCustom;
