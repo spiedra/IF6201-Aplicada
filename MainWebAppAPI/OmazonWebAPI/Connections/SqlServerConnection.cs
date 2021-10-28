deff60c2c1245ac459cada0838c40aa14d905c21
Randall <randms07@hotmail.com>
2021-10-27 19:10:24 -0600

    Iconfiguration singleton added}

--- MainWebAppAPI/OmazonWebAPI/Connections/SqlServerConnection.cs
+++ MainWebAppAPI/OmazonWebAPI/Connections/SqlServerConnection.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +10,57 @@ namespace OmazonWebAPI.Connections
 {
     public class SqlServerConnection
     {
-        //
+        public SqlCommand SqlCommand { get; set; }
+        public SqlConnection SqlConnection { get; set; }
+        public SqlDataReader SqlDataReader { get; set; }
+        public SqlParameter ParameterReturn { get; set;}
+
+        public SqlServerConnection(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+      
+
+        public void InitSqlComponents(string commandText)
+        {
+            this.SqlConnection = new SqlConnection(Configuration.GetConnectionString("DB_Connection_Provider1"));
+            this.SqlCommand = new SqlCommand(commandText, this.SqlConnection);
+        }
+
+        public void CreateParameter(string parameterName, SqlDbType dbType, object value)
+        {
+            SqlParameter sqlParameter = new(parameterName, dbType);
+            sqlParameter.Value = value;
+            this.SqlCommand.Parameters.Add(sqlParameter);
+        }
+
+        public void CreateParameterOutput()
+        {
+            this.ParameterReturn = new SqlParameter();
+            ParameterReturn.Direction = ParameterDirection.ReturnValue;
+            this.SqlCommand.Parameters.Add(this.ParameterReturn);
+        }
+
+        public void ExecuteNonQuery()
+        {
+            this.ExecuteConnectionCommands();
+            this.SqlCommand.ExecuteNonQuery();
+            this.SqlConnection.Close();
+        }
+
+        public void ExcecuteReader()
+        {
+            this.ExecuteConnectionCommands();
+            this.SqlDataReader = this.SqlCommand.ExecuteReader();
+        }
+
+        public void ExecuteConnectionCommands()
+        {
+            this.SqlConnection.Open();
+            this.SqlCommand.CommandType = CommandType.StoredProcedure;
+        }
     }
 }
